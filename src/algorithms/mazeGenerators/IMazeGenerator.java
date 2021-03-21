bdea1e922d5ed5e0efbed096b315232a4168912a
or-fadlon <or5690@gmail.com>
2021-03-21 16:56:15 +0200

    java docs for the methods

--- src/algorithms/mazeGenerators/IMazeGenerator.java
+++ src/algorithms/mazeGenerators/IMazeGenerator.java
@@ -1,7 +1,19 @@
 package algorithms.mazeGenerators;
 
 public interface IMazeGenerator {
+    /***
+     * generate a maze
+     * @param rows number of rows of the maze to generate
+     * @param columns number of columns of the maze to generate
+     * @return generated maze
+     */
     Maze generate(int rows, int columns);
 
+    /***
+     * measure the time to generate maze algorithm
+     * @param rows number of rows of the maze to generate
+     * @param columns number of columns of the maze to generate
+     * @return time takes to generate in Milli seconds
+     */
     long measureAlgorithmTimeMillis(int rows, int columns);
 }
