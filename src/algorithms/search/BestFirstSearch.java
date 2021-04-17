6661580cdc92114e09a9454b2dc6e8d33a0db2ba
or-fadlon <or5690@gmail.com>
2021-04-17 13:32:44 +0300

    some docs added

--- src/algorithms/search/BestFirstSearch.java
+++ src/algorithms/search/BestFirstSearch.java
@@ -12,10 +12,21 @@ public class BestFirstSearch extends BreadthFirstSearch {
     }
 }
 
+/**
+ * Comparator for the PriorityQueue
+ */
 class priceComparator implements Comparator<AState> {
 
+    /**
+     * @param o1 AState to compare
+     * @param o2 AState to compare
+     * @return =0 - equals, >0 - o1 is bigger, <0 - o2 is bigger
+     * @throws IllegalArgumentException one or more of the given AState is Null
+     */
     @Override
-    public int compare(AState o1, AState o2) {
+    public int compare(AState o1, AState o2) throws IllegalArgumentException {
+        if (o1 == null || o2 == null)
+            throw new IllegalArgumentException("one or more of the given AState is Null");
         return Integer.compare(o1.getCost(), o2.getCost());
     }
 }
