85617775bd2cccc4004c8a374cc54cc5870c71d2
or-fadlon <or5690@gmail.com>
2021-05-08 17:34:59 +0300

    implement my compress

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -5,14 +5,63 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class MyCompressorOutputStream extends OutputStream {
-    private OutputStream out;
+    private final OutputStream out;
 
     public MyCompressorOutputStream(OutputStream out) {
         this.out = out;
     }
 
+    private static byte[] compress(byte[] bytes) {
+        ArrayList<Byte> holder = new ArrayList<>(); //placeholder for our return array
+
+        //insert meta data
+        for (int i = 0; i < 12; i++)
+            holder.add(bytes[i]);
+
+        //compress maze
+        byte[] temp = new byte[8];
+        for (int i = 12; i < bytes.length; i += 8) {
+            for (int j = 0; j < 8; j++)
+                temp[j] = i + j < bytes.length ? bytes[i + j] : 0;
+            holder.add(toOneUnsignedByte(temp));
+        }
+
+        //prepare the result array
+        byte[] gridAsArray = new byte[holder.size()];
+        for (int i = 0; i < gridAsArray.length; i++) {
+            gridAsArray[i] = holder.get(i);
+        }
+        return gridAsArray;
+    }
+
+    private static byte toOneUnsignedByte(byte[] array) throws IllegalArgumentException {
+        if (array.length > 8)
+            throw new IllegalArgumentException("The given array length is bigger then 8");
+        int sum = 0;
+        for (int i = 0; i < array.length; i++)
+            sum += array[i] * Math.pow(2, i);
+        return (byte) sum;
+    }
+
     @Override
     public void write(int b) throws IOException {
-        this.out.write(b);
+        //TODO: what we need to implement here????
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        this.out.write(compress(b));
+    }
+
+    @Override
+    public void flush() throws IOException {
+        this.out.flush();
+        super.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.out.close();
+        super.close();
     }
 }
--- src/IO/MyDecompressorInputStream.java
+++ src/IO/MyDecompressorInputStream.java
@@ -2,6 +2,7 @@ package IO;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MyDecompressorInputStream extends InputStream {
     private InputStream in;
@@ -10,8 +11,47 @@ public class MyDecompressorInputStream extends InputStream {
         this.in = in;
     }
 
+    private static int decompress(byte[] bytes, byte[] target) throws IllegalArgumentException {
+        ArrayList<Byte> holder = new ArrayList<>(); //placeholder for our return array
+
+        //insert meta data
+        for (int i = 0; i < 12; i++)
+            holder.add(bytes[i]);
+
+        //check target match the expected
+        int expectedSize = (holder.get(0) + holder.get(1) * 127) * (holder.get(2) + holder.get(3) * 127) + 12;
+        if (target.length < expectedSize)
+            throw new IllegalArgumentException("the target byte Array given in too small");
+
+        //compress maze
+        for (int i = 12; i < bytes.length; i++) {
+            int temp = bytes[i] > 0 ? bytes[i] : bytes[i] + 256;
+            for (int j = 0; j < 8; j++) {
+                holder.add((byte) (temp % 2));
+                temp /= 2;
+            }
+        }
+
+
+        for (int i = 0; i < target.length; i++)
+            target[i] = holder.get(i);
+
+        return 1;
+    }
+
     @Override
-    public byte[] read() throws IOException {
-        return null;
+    public int read() throws IOException {
+        return 1;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException, IllegalArgumentException {
+        return decompress(in.readAllBytes(), b);
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.in.close();
+        super.close();
     }
 }
