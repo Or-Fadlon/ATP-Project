92046e4a11fd7e54dc29acd6f4e261fcd0db898e
or-fadlon <or5690@gmail.com>
2021-05-08 16:58:24 +0300

    update after some changes

--- src/IO/SimpleCompressorOutputStream.java
+++ src/IO/SimpleCompressorOutputStream.java
@@ -11,47 +11,81 @@ public class SimpleCompressorOutputStream extends OutputStream {
         this.out = out;
     }
 
+    /**
+     * compress byte array that represent a Maze.
+     * count series of 1 or 0 and change it to the counter.
+     * 255,0,15,6 - 255+15 times 1 and then 6 times 0.
+     *
+     * @param bytes first 12 cells are the meta-data. other are the maze details
+     * @return compressed array
+     */
+    private static byte[] compress(byte[] bytes) {
+        ArrayList<Byte> holder = new ArrayList<>(); //placeholder for our return array
+        int counter = 0, //count the number of times Type appear in a sequence
+                countingType = 1; //1-wall, 0-tile
+        //insert meta data
+        for (int i = 0; i < 12; i++)
+            holder.add(bytes[i]);
+        //compress maze
+        for (int i = 12; i < bytes.length; i++) {
+            if (bytes[i] == countingType)
+                counter++;
+            else {
+                while (counter >= 0) { //handle the insertion
+                    if (counter > 255) {
+                        holder.add(toUnsignedByte(255));
+                        holder.add((byte) 0);
+                    } else
+                        holder.add(toUnsignedByte(counter));
+                    counter -= 255;
+                }
+                countingType = countingType == 1 ? 0 : 1; //change type
+                counter = 1;
+            }
+        }
+        holder.add(toUnsignedByte(counter));
+        //prepare the result array
+        byte[] gridAsArray = new byte[holder.size()];
+        for (int i = 0; i < gridAsArray.length; i++)
+            gridAsArray[i] = holder.get(i);
+
+        return gridAsArray;
+    }
+
+    /**
+     * convert normal number into "unsigned-byte like"
+     * can convert only numbers between 0-255.
+     *
+     * @param num number between 0-255
+     * @return positive number represent the same value, negative numbers represent (num*-1)+127
+     * @throws IllegalArgumentException can get only number between 0-255
+     */
+    private static byte toUnsignedByte(int num) {
+        byte result;
+        if (num < 0 || 255 < num)
+            throw new IllegalArgumentException("can get only number between 0-255");
+        return (byte) num;
+    }
+
     @Override
     public void write(int b) throws IOException {
-
+        //TODO: what we need to implement here????
     }
 
     @Override
     public void write(byte[] b) throws IOException {
-        //TODO
-        this.out.write(this.compress(b));
+        this.out.write(compress(b));
     }
 
-    private byte[] compress(byte[] b) {
-        ArrayList<Byte> holder = new ArrayList<>();
-        int counter = 0, countingType = 1; //1-wall, 0-tile
+    @Override
+    public void flush() throws IOException {
+        this.out.flush();
+        super.flush();
+    }
 
-        //insert meta data
-        for (int i = 0; i < 12; i++)
-            holder.add(b[i]);
-
-        //compress maze
-        for (int i = 12; i < b.length; i++) {
-            if (b[i] == countingType)
-                counter++;
-            else {
-                while (counter >= 0) {
-                    if (counter > 255) {
-                        holder.add((byte) 255);
-                        holder.add((byte) 0);
-                    } else
-                        holder.add((byte) counter);
-                    counter -= 255;
-                }
-                countingType = countingType == 1 ? 0 : 1;
-                counter = 0;
-            }
-        }
-
-        byte[] gridAsArray = new byte[holder.size()];
-        for (int i = 0; i < gridAsArray.length; i++) {
-            gridAsArray[i] = holder.get(i);
-        }
-        return gridAsArray;
+    @Override
+    public void close() throws IOException {
+        this.out.close();
+        super.close();
     }
 }
--- src/IO/SimpleDecompressorInputStream.java
+++ src/IO/SimpleDecompressorInputStream.java
@@ -2,6 +2,7 @@ package IO;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class SimpleDecompressorInputStream extends InputStream {
     private InputStream in;
@@ -10,8 +11,69 @@ public class SimpleDecompressorInputStream extends InputStream {
         this.in = in;
     }
 
+    /**
+     * decompress byte array that represent a Maze.
+     * 255,0,15,6 - 255+15 times 1 and then 6 times 0.
+     *
+     * @param b      input array to decompress
+     * @param target in-place changes the values to the decompress form of the input
+     * @return 0 - fail, 1 - success //TODO: is this the right way to handle this????
+     * @throws IllegalArgumentException the target byte Array given in too small
+     */
+    private static int decompress(byte[] b, byte[] target) throws IllegalArgumentException {
+        ArrayList<Byte> holder = new ArrayList<>(); //placeholder for our return array
+        byte countingType = 1; //1-wall, 0-tile
+        //insert meta data
+        for (int i = 0; i < 12; i++)
+            holder.add(b[i]);
+
+        //check target match the expected
+        int expectedSize = (holder.get(0) + holder.get(1)*127) * (holder.get(2) + holder.get(3)*127) + 12;
+        if (target.length < expectedSize)
+            throw new IllegalArgumentException("the target byte Array given in too small");
+
+        //compress maze
+        for (int i = 12; i < b.length; i++) {
+            int value = unsignedByteToInt(b[i]);
+            for (int j = 0; j < value; j++)
+                holder.add(countingType);
+            countingType = countingType == 1 ? (byte) 0 : 1; //change type
+        }
+
+        for (int i = 0; i < holder.size(); i++)
+            target[i] = holder.get(i);
+
+        return 1;
+    }
+
+    /**
+     * convert "unsigned-byte like" number into normal one.
+     *
+     * @param num number in "unsigned-byte like" format
+     * @return positive number represent the same value, negative numbers represent (num*-1)+127
+     */
+    private static int unsignedByteToInt(byte num) {
+        int result;
+        if (num >= 0)
+            result = num;
+        else
+            result = num + 256;
+        return result;
+    }
+
     @Override
-    public byte[] read() throws IOException {
-        return null;
+    public int read() throws IOException {
+        return 0;
+    }
+
+    @Override
+    public int read(byte[] b) throws IOException, IllegalArgumentException {
+        return decompress(in.readAllBytes(), b);
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.in.close();
+        super.close();
     }
 }
